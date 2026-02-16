325981366c4f8994bf5e1365be7b73f7fd8e9375
tasniamaha <tasniamaha17march@gmail.com>
2026-02-17 01:58:06 +0600

    json parse

--- Lab_2/src/index.ts
+++ Lab_2/src/index.ts
@@ -14,5 +14,14 @@ fs.readFile(filepath,'utf-8',(err,data)=>{
     if(err){
         return console.error('Error',err.message)
     }
-    console.log(data)
+    //parse json
+    const user=JSON.parse(data)
+
+    for(const index in user){
+       const username=user[index].name
+       const age=user[index].age
+       console.log(`Hello ${username},Your age is ${age}`)
+       //console.log(`Hello ${user[index].name}`)
+       //console.log(`Your age is ${age}`)
+    }
 })
