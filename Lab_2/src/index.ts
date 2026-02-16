c8db96aa61334c5440546799d610fca60d069b1c
tasniamaha <tasniamaha17march@gmail.com>
2026-02-17 01:27:28 +0600

    feat:read data from files

--- Lab_2/src/index.ts
+++ Lab_2/src/index.ts
@@ -1,2 +1,18 @@
+import fs from 'fs'
+import path from 'path'
 
-console.log("Hello, World!");
+const file=process.argv[2]
+
+if(!file){
+    console.log('Provide file name')
+    process.exit(1)
+}
+
+const filepath=path.resolve(file)
+
+fs.readFile(filepath,'utf-8',(err,data)=>{
+    if(err){
+        return console.error('Error',err.message)
+    }
+    console.log(data)
+})
